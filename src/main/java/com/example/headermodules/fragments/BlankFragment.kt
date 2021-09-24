a1605f89fc178e3e6f381e1536f0fbee7cea2801
Todd Greenberg <tsgreenberg1217@gmail.com>
2021-09-24 14:20:27 -0400

    Update BlankFragment.kt

--- src/main/java/com/example/headermodules/fragments/BlankFragment.kt
+++ src/main/java/com/example/headermodules/fragments/BlankFragment.kt
@@ -2,4 +2,4 @@ package com.example.headermodules.fragments
 import com.example.headermodules.baseClasses.BaseFragment
 import com.example.headermodules.R
 
-class BlankFragment: BaseFragment(R.layout.blank_fragment) // this is a blank fragment
+class BlankFragment: BaseFragment(R.layout.blank_fragment) // this is a blank fragment, more comments
