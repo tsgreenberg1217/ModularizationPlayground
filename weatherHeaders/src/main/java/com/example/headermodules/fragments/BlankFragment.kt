0ddf37dc0dd5329f01a68d0916ab6a595a2e89dd
Todd Greenberg <tsgreenberg1217@gmail.com>
2021-09-24 00:18:12 -0400

    change 1

--- weatherHeaders/src/main/java/com/example/headermodules/fragments/BlankFragment.kt
+++ weatherHeaders/src/main/java/com/example/headermodules/fragments/BlankFragment.kt
@@ -3,3 +3,4 @@ import com.example.headermodules.baseClasses.BaseFragment
 import com.example.headermodules.R
 
 class BlankFragment: BaseFragment(R.layout.blank_fragment) // this is a blank fragment
+// this is another comment
