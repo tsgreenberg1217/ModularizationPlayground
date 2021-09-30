7d2caf0e93baa92bffddcfd2178188c437d7795d
Todd Greenberg <tsgreenberg1217@gmail.com>
2021-09-30 17:14:46 -0400

    Squashed 'weathermodule/' content from commit 99d0590
    git-subtree-dir: weathermodule
    git-subtree-split: 99d0590eff8b5b4724e5c0ee002109a080407a48

---
+++ src/main/java/com/example/headermodules/fragments/Fragment_2.kt
@@ -0,0 +1,47 @@
+package com.example.headermodules.fragments
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.data_utility.DataState
+// some comment
+import com.example.data_utility.models.CityWeatherResult
+import com.example.headermodules.R
+import com.example.headermodules.recyclerViews.WeatherRecyclerViewAdapter
+import com.example.headermodules.baseClasses.BaseFragment
+import com.example.headermodules.databinding.Fragment2Binding
+import com.example.headermodules.viewmodels.WeatherViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import java.util.*
+
+@AndroidEntryPoint
+class Fragment_2 : BaseFragment(R.layout.fragment_2) {
+    lateinit var binding: Fragment2Binding
+
+    private val weatherViewModel: WeatherViewModel by viewModels()
+    lateinit var adapter: WeatherRecyclerViewAdapter
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding = Fragment2Binding.bind(view)
+        binding.weatherRV.layoutManager = LinearLayoutManager(this@Fragment_2.context)
+        adapter = WeatherRecyclerViewAdapter(mutableListOf())
+        binding.weatherRV.adapter = adapter
+        weatherViewModel.cities.observe(viewLifecycleOwner) {
+            when (it) {
+                is DataState.Success -> {
+                    adapter.updateCities(it.data)
+                }
+                is DataState.Error -> {
+                }
+                is DataState.Loading -> {
+                }
+                else -> {
+                }
+            }
+        }
+        weatherViewModel.getAllCities()
+    }
+}
